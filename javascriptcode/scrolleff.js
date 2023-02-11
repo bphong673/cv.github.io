b4ed957e8c8e02ff39c73c80933658e92173a2c2
bphong673 <bphong673@gmail.com>
2023-02-11 15:15:40 +0700

    newupdate

--- javascriptcode/scrolleff.js
+++ javascriptcode/scrolleff.js
@@ -4,26 +4,54 @@ var navheight = document.getElementById('nav').clientHeight + document.getElemen
 var scroll_timeline = document.body.scrollTop;
 
 
-console.log(navheight);
+function checkVisible(elm) {
+    var rect = elm.getBoundingClientRect();
+    var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+    return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+  }
+
+
 function change_scroll_timeLine() {
 
-    var winheight = document.documentElement.scrollHeight;
-    var scrollPos = document.body.scrollTop
-
-    scroll_timeline = scrollPos / winheight * 100;
-    console.log(scroll_timeline);
-
     
+const scrollTop = document.body.scrollTop;
+const scrollHeight = document.body.scrollHeight;
+const clientHeight = document.body.clientHeight;
+
+scroll_timeline = Math.floor(scrollTop / (scrollHeight - clientHeight) * 100 + 0.1)
+
+var animatepart = document.getElementById('animate')
+ const areVisible = checkVisible(animatepart);
+ if (areVisible == true)  {
+    console.log(animatepart.getBoundingClientRect());
+
+    console.log(2>=4/2)
+ };
+
+
+
+   
+
 };
 
-function lenghthen_div(id, width){
+function lenghthen_div(id, width) {
     var target = document.getElementById(id);
     target.style.width = width + '%';
-    console.log(id);
 };
 
-document.body.addEventListener('scroll', ()=>{
+document.body.addEventListener('scroll', () => {
     change_scroll_timeLine();
     lenghthen_div('progressbar', scroll_timeline);
 }
 );
+
+
+
+
+
+const friends = ['Thang', 'Phat',   'Thao       ', 'Thai', 'Nghi', 'Yiii', 'Damn'] ;
+
+const firstLetter = friends.map(friend => friend[0] + friend[1]);
+console.log(firstLetter);
+
+
